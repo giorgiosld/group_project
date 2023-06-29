3c2865710e273d9c157454636c77c4cb126e1b59
giorgio <giorgiosaldana99@gmail.com>
2023-06-29 23:16:26 +0200

    Setup ssrf attack

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from src.db import result_query
 from src.schema import create_db
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -15,13 +16,13 @@ def index():
          <li><a href="/commandinjection">Command Injection</a></li>
          <li>Directory Traversal</li>
          <li>Cross Site Scripting</li>
-         <li>Server Side Requests Forgery</li>
+         <li><a href="/ssrf">Server Side Requests Forgery</a></li>
       </ul>/home/giorgiosld/university/bachelor/group_project
    '''
    return about
 
 @app.get('/sqli/<name>')
-def query(name):
+def sqli(name):
    print("-" * 50)
    print(f"Passing input: {name}")
 
@@ -36,17 +37,13 @@ def query(name):
    return f"{disclaimer}<br/><h3>Results</h3><ol>{''.join(output)}</ol>"
 
 @app.get('/commandinjection')
-def search_files():
+def command_injection():
    form = f"""
-   <html>
-      <body>
    <form action = "commandinjection" method = "GET">
       <p><h3>Enter a file to search into server</h3></p>
       <p><input type = 'text' name = 'filename'/></p>
       <p><input type = 'submit' value = 'Search'/></p>
    </form>
-      </body>
-   </html>
    """
    if request.args.get('filename'):
       filename = request.args.get('filename')
@@ -54,6 +51,20 @@ def search_files():
       output = os.popen(command).read()
       return f"{form}\nRequested file '{filename}' is present at: {output}"
       
+   return form
+
+@app.get('/ssrf')
+def ssrf():
+   form = f"""
+   <form action = "ssrf" method = "GET">
+      <p><h3>Enter a file to search into server</h3></p>
+      <p><input type = 'text' name = 'url'/></p>
+      <p><input type = 'submit' value = 'Search'/></p>
+   </form>
+   """
+   if request.args.get('url'):
+      url = request.args.get('url')
+      return requests.get(url).text
    return form
 
 if __name__ == '__main__':
